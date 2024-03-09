4cc8a704ab78ff5481b57c087e34e16d92e98fb1
Juan Jose Chiroque <juanjosechiroque@gmail.com>
2024-03-08 21:44:04 -0500

    feat: delete dist folder

--- dist/posts/posts.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const posts_controller_1 = require("./posts.controller");
-const router = (0, express_1.Router)();
-router.get("/", posts_controller_1.getPosts);
-router.post("/", posts_controller_1.createPost);
-exports.default = router;
